773f5273525500bc2139b798d72f6a3941c84b33
felipecustodio <felipe.crochi@gmail.com>
2015-10-20 14:42:23 -0200

    Basic heap functions

--- heap/includes/heap.h
+++ heap/includes/heap.h
@@ -12,3 +12,27 @@
 	(c) Felipe Scrochio Custódio
 ---------------------------------------------------------*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define left_child 0
+#define right_child 1
+
+typedef struct node {
+
+	int value;
+	NODE *left;
+	NODE *right;
+
+} NODE;
+
+typedef struct heap {
+
+	NODE *root;
+
+} HEAP;
+
+HEAP* createHeap();
+NODE* createRoot(HEAP* tree, int content);
+NODE* insertChild(int child, NODE *node, int content);
+
--- heap/src/heap.c
+++ heap/src/heap.c
@@ -11,3 +11,54 @@
 				HEAP
 	(c) Felipe Scrochio Custódio
 ---------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "heap.h"
+
+HEAP* createHeap() {
+
+	HEAP* tree = (HEAP*)malloc(sizeof(TREE));
+	
+	if (tree != NULL) {
+		tree->root = NULL;
+	}
+
+	return tree;
+
+}
+
+NODE* createRoot(HEAP* tree, int content) {
+
+	tree->root = (NODE*)malloc(sizeof(NODE));
+	if(tree->root != NULL) {
+		tree->root->right = NULL;
+		tree->root->left = NULL;
+		tree->root->value = content;
+	}
+
+	return (tree->root);
+
+}
+
+NODE* insertChild(int child, NODE *node, int content) {
+
+	NODE *new = (NODE*)malloc(sizeof(NODE));
+
+	if(new != NULL) {
+
+		new->right = NULL;
+		new->left = NULL;
+		new->value = content;
+
+		if (child == left_child) {
+			node->left = new;
+		} else {
+			node->right = new;
+		}
+
+	}
+
+	return new;
+
+}
